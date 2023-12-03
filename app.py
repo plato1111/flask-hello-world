57f23fe5013f8303451868ecce3ca8ca36fd6081
plato1111 <64042307+plato1111@users.noreply.github.com>
2023-12-03 19:35:57 +0000

    jj1

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!1112'
+    return 'Hello, World!1112', render_template('index.html')
